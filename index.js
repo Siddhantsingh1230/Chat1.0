b3ff0ff6f09068395da9caa29619e0bf3ae6b0ea
Siddhant Singh <siddhantsingh9023134084@gmail.com>
2022-12-24 00:20:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,192 @@
+let title = document.title;
+let appkey;
+let itemkey;
+function askKey() {
+  appkey = prompt("        Enter Appkey");
+  if (appkey == null || appkey == "") {
+    alert("Error (Appkey) 🚫");
+    document.location.reload();
+  } else {
+    itemkey = prompt("        Enter Your Key");
+    if (itemkey == null || itemkey == "") {
+      alert("Error (Item Key)❌ ");
+      document.location.reload();
+    }
+  }
+}
+window.addEventListener("blur", () => {
+  document.title = "See You Again";
+});
+window.addEventListener("focus", () => {
+  document.title = title;
+});
+function uptype() {
+  let appkey = "2aiei0jz";
+
+  let itemkey = "up";
+
+  let type = document.getElementById("input");
+
+  let itemval;
+
+  if (type.innerHTML) {
+    itemval = 1;
+  } else {
+    itemval = 0;
+  }
+
+  console.log(itemval);
+
+  $.ajax({
+    type: "POST",
+
+    url:
+      "https://keyvalue.immanuel.co/api/KeyVal/UpdateValue/" +
+      appkey +
+      "/" +
+      itemkey +
+      "/" +
+      itemval,
+
+    contentType: false,
+
+    processData: false,
+  })
+
+    .done(function (data) {})
+
+    .fail(function (err) {});
+}
+
+function downtype() {
+  let type = document.getElementsByClassName("bouncing-loader");
+
+  fetch("https://keyvalue.immanuel.co/api/KeyVal/GetValue/dimip0w6/down")
+    .then((response) => response.json())
+
+    .then((data) => {
+      if (data == 1) {
+        type[0].style.visibility = "visible";
+      } else {
+        type[0].style.visibility = "hidden";
+      }
+
+      console.log(data);
+    });
+}
+
+function updateScroll() {
+  var element = document.getElementById("msg");
+
+  element.scrollTop = element.scrollHeight;
+}
+
+function updateScrolltop() {
+  var element = document.getElementById("msg");
+
+  element.scrollTop = 0;
+}
+
+function getValue(appkey, itemkey) {
+  $.ajax({
+    type: "GET",
+
+    url:
+      "https://keyvalue.immanuel.co/api/KeyVal/GetValue/" +
+      appkey +
+      "/" +
+      itemkey,
+
+    contentType: false,
+
+    processData: false,
+  })
+
+    .done(function (data) {
+      let msg = document.getElementById("msg");
+
+      const lineBreak = document.createElement("br");
+
+      msg.append(data);
+
+      msg.appendChild(lineBreak);
+
+      updateScroll();
+    })
+
+    .fail(function (err) {});
+}
+
+function updateValue(appkey, itemkey, itemval) {
+  $.ajax({
+    type: "POST",
+
+    url:
+      "https://keyvalue.immanuel.co/api/KeyVal/UpdateValue/" +
+      appkey +
+      "/" +
+      itemkey +
+      "/" +
+      itemval,
+
+    contentType: false,
+
+    processData: false,
+  })
+
+    .done(function (data) {
+      getValue(appkey, yourkey);
+    })
+
+    .fail(function (err) {});
+}
+
+function update() {
+  let inp = document.getElementById("input");
+
+  itemval = inp.innerText;
+
+  updateValue(appkey, itemkey, itemval.trim());
+
+  inp.innerHTML = "";
+}
+let old;
+
+setInterval(function () {
+  fetch("https://keyvalue.immanuel.co/api/KeyVal/GetValue/2y8etr45/khu")
+    .then((response) => response.json())
+
+    .then((data) => {
+      console.log(old);
+
+      let msg = document.getElementById("msg");
+
+      const lineBreak = document.createElement("br");
+
+      if (old != data && typeof data === "string") {
+        msg.append("[");
+
+        msg.append(data);
+
+        msg.append("]");
+
+        msg.appendChild(lineBreak);
+
+        updateScroll();
+      }
+
+      old = data;
+    });
+
+  downtype();
+
+  uptype();
+}, 1000);
+window.addEventListener("keydown", function (e) {
+  console.log(e.key);
+  if (e.keyCode == 13) {
+    event.preventDefault();
+    console.log("entered");
+    update();
+  }
+});
